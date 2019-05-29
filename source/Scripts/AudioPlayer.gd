577a455ae7af2dffab14e79f11def12ba693dec3
nightblade9 <nightbladecodes@gmail.com>
2019-05-29 03:10:51 -0400

    Streaming WORKS!!! But all at once.

--- source/Scripts/AudioPlayer.gd
+++ source/Scripts/AudioPlayer.gd
@@ -5,19 +5,64 @@ var item:Dictionary
 func _ready():
 	$PlayButton.disabled = true
 	$PlayButton.text = "Loading ..."
-	# TODO: doesn't seem possible to stream, just to get the whole file
-	var request = HTTPRequest.new()
-	add_child(request)
-	request.connect("request_completed", self, "_on_audio_ready")
-	request.request(item.url)
-
-func _on_audio_ready(result, response_code, headers, body):
-	$PlayButton.disabled = false
-	$PlayButton.text = "Play"
 	
-	var ogg = AudioStreamOGGVorbis.new()
-	ogg.data = body
-	$AudioStreamPlayer.stream = ogg
+	var start = item.url.find("://") + 3
+	var stop = item.url.find("/", start)
+	var host = item.url.substr(start, stop - start)
+	var use_ssl = item.url.find("https://") > -1
+	
+	var url = item.url.substr(stop, len(item.url))
+	
+	# Stream the file
+	var http = HTTPClient.new()
+	var error = http.connect_to_host(host, -1, use_ssl)
+	
+	var ogg_stream = AudioStreamOGGVorbis.new()
+	var rb = PoolByteArray()
+	
+	############################################################
+	# http://codetuto.com/2015/05/using-httpclient-in-godot/
+	
+	while(http.get_status() != HTTPClient.STATUS_CONNECTED):
+		http.poll()
+		OS.delay_msec(100)
+
+	var headers = [
+		"User-Agent: Pirulo/1.0 (Godot)",
+		"Accept: */*"
+	]
+
+	var status = http.get_status()
+	var expected = HTTPClient.STATUS_CONNECTED
+
+	# TODO: do everything below in a background thread as we play
+	error = http.request(HTTPClient.METHOD_GET, url, headers)
+
+	while (http.get_status() == HTTPClient.STATUS_REQUESTING):
+		http.poll()
+		OS.delay_msec(100)
+
+	if(http.has_response()):
+		headers = http.get_response_headers_as_dictionary()
+		while(http.get_status() == HTTPClient.STATUS_BODY):
+			http.poll()
+			var chunk = http.read_response_body_chunk()	
+			if(chunk.size() == 0):
+				OS.delay_usec(100)
+			else:
+				# Updated every chunk, meh.
+				$PlayButton.disabled = false
+				$PlayButton.text = "Play"
+				
+				rb = rb + chunk
+				
+				# constantly re-assign
+				ogg_stream.data = rb
+				$AudioStreamPlayer.stream = ogg_stream
+				#call_deferred("_send_loading_signal",rb.size(),http.get_response_body_length())
+	  
+	pass
+	############################################################
 
 func _on_PlayButton_pressed():
 	$AudioStreamPlayer.play()
