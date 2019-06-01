ea5bb9288d98582a6b5299c1ac26c094eb2b17a5
nightblade9 <nightbladecodes@gmail.com>
2019-06-01 02:58:38 -0400

    Convert buttons into item list - not wired up yet

--- source/Scripts/Index.gd
+++ source/Scripts/Index.gd
@@ -10,27 +10,20 @@ func _ready():
 	request.request(_INDEX_FILE_URL)
 
 func _on_download_completed(result, response_code, headers, body):
-	$Label.text = ""
+	$StatusLabel.text = ""
 	var json = JSON.parse(body.get_string_from_utf8()).result
-	var i = 0
 	
 	for item in json:
 		var button = Button.new()
 		button.text = item.title + " (" + str(item.duration_minutes) + " minutes)"
 		button.connect("button_down", self, "_button_clicked", [item])
 		button.add_font_override("font", load("res://DefaultFont.tres"))
-		
-		button.margin_left = 16
-		button.margin_top += 16 + (i * 32)
-		button.margin_bottom += 16 + (i * 16)
-		
-		add_child(button)
-		i += 1
+		$ItemList.add_item(item.title)
 
-func _button_clicked(item):
-	var player = AudioPlayer.instance()
-	player.item = item
-	add_child(player)
-	player.popup_exclusive = true
-	player.popup_centered()
+#func _button_clicked(item):
+#	var player = AudioPlayer.instance()
+#	player.item = item
+#	add_child(player)
+#	player.popup_exclusive = true
+#	player.popup_centered()
 	
